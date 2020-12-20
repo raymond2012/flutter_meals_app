09df05020c2cbf0c8f3ebdaf5cff8f6b0e9af110
Raymond <raymond96521>
2020-12-20 22:42:56 +0800

    Passing data with Named Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/categoy_meals_screen.dart';
 import './catagories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -29,6 +30,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
